a86db9b5144ce3cfa840c779cb05efbe419353e3
sanek1 <sanek-1315@mail.ru>
2018-02-02 12:46:29 +0300

    поправил тест

--- ProjectJob/TestJob/src/test/java/WorkDataTest.java
+++ ProjectJob/TestJob/src/test/java/WorkDataTest.java
@@ -9,8 +9,8 @@ public class WorkDataTest {
         int[] actual = data.SortString("1;2;3;4;5;6;7;8;9;10");
         int[] expected = new int[]{2,4,6,8,10};
 
-        int[] actual2 = data.SortString("1;2;3;4;5;6;7;8;9;10");
-        int[] expected2 = new int[]{2,4,6,8,10};
+        int[] actual2 = data.SortString("34;45;52352;323;32");
+        int[] expected2 = new int[]{32,34,52352};
 
 
         assertArrayEquals(expected,actual);
